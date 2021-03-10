1ea5f46042802d0954a3a3f639372d130a365613
Mohamed <mimouni455@gmail.com>
2021-03-10 09:14:10 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Calcule1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calcule1.dir/main.c.o"
+  "Calcule1"
+  "Calcule1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Calcule1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
